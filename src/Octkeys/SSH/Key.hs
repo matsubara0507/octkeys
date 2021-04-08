281a8797aeb49fd4b11bb68985779178b2720848
MATSUBARA Nobutada <nobutada0507@gmail.com>
2021-04-09 01:07:25 +0900

    Feat: can use sha256/base64 fingerprint

--- src/Octkeys/SSH/Key.hs
+++ src/Octkeys/SSH/Key.hs
@@ -1,13 +1,26 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Octkeys.SSH.Key where
 
 import           RIO
 import qualified RIO.ByteString          as B
+import qualified RIO.Text                as T
 
-import           Crypto.Hash             (Digest, MD5)
+import           Crypto.Hash             (Digest, HashAlgorithm, MD5, SHA256)
 import qualified Crypto.Hash             as Crypto
-import           Data.ByteArray.Encoding (Base (Base64), convertFromBase)
+import           Data.ByteArray.Encoding (Base (Base64), convertFromBase,
+                                          convertToBase)
 
-fingerprint :: ByteString -> Maybe (Digest MD5)
+md5 :: ByteString -> Maybe String
+md5 = fmap show . (fingerprint @ MD5)
+
+sha256 :: ByteString -> Maybe String
+sha256 = fmap (filter (/= '=') . T.unpack . textDisplay . displayBytesUtf8 . encode) . fingerprint
+  where
+    encode :: Digest SHA256 -> ByteString
+    encode = convertToBase Base64
+
+fingerprint :: HashAlgorithm a => ByteString -> Maybe (Digest a)
 fingerprint content = do
   body <- pubkeyBody content
   bin  <- decode body
--- test/Spec/Octkeys/SSH/Key.hs
+++ test/Spec/Octkeys/SSH/Key.hs
@@ -8,26 +8,43 @@ import           Test.Tasty.Hspec
 
 spec :: Spec
 spec = do
-  describe "fingerprint" $ do
+  describe "md5" $ do
     context "when apply correct ssh public key" $
       it "should return Just fingerprint" $
-        show <$> SSHKey.fingerprint sshkey `shouldBe` Just (filter (/= ':') fp)
+        SSHKey.md5 sshkey `shouldBe` Just (filter (/= ':') md5fp)
     context "when apply incorrect header publick key" $ do
       let sshkey' = B.drop 1 sshkey
       it "should return Nothing" $
-        show <$> SSHKey.fingerprint sshkey' `shouldBe` Nothing
+        SSHKey.md5 sshkey' `shouldBe` Nothing
     context "when apply public key without header" $ do
       let sshkey' = B.drop (B.length "ssh-rsa ") sshkey
       it "should return Nothing" $
-        show <$> SSHKey.fingerprint sshkey' `shouldBe` Nothing
+        SSHKey.md5 sshkey' `shouldBe` Nothing
     context "when incorrect public key (no base64)" $ do
       let sshkey' = sshkey <> "1"
       it "should return Nothing" $
-        show <$> SSHKey.fingerprint sshkey' `shouldBe` Nothing
+        SSHKey.md5 sshkey' `shouldBe` Nothing
+  describe "sha256" $ do
+    context "when apply correct ssh public key" $
+      it "should return Just fingerprint" $
+        SSHKey.sha256 sshkey `shouldBe` Just sha256
+    context "when apply incorrect header publick key" $ do
+      let sshkey' = B.drop 1 sshkey
+      it "should return Nothing" $
+        SSHKey.sha256 sshkey' `shouldBe` Nothing
+    context "when apply public key without header" $ do
+      let sshkey' = B.drop (B.length "ssh-rsa ") sshkey
+      it "should return Nothing" $
+        SSHKey.sha256 sshkey' `shouldBe` Nothing
+    context "when incorrect public key (no base64)" $ do
+      let sshkey' = sshkey <> "1"
+      it "should return Nothing" $
+        SSHKey.sha256 sshkey' `shouldBe` Nothing
 
 
 sshkey :: ByteString
 sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDlpea3sCHIlkER6XA1ObytOWK/YTEEdkFtbMuDyrsui/cLXEFEkCotLz30uzER4fR0hJH0Ro/LbwaE7hvJkxH414+leSnUld7jchFUzZa2KPREyETBv0w0hliI+4MFm6e9zyr0hWBDOO4HFg4dMDuY2x4uXCHeAEVTXWlB23hqSoJBQLbNh/zFMzmjMP1DViA1gIVZoVeNfYEnL/eEmMY3wSo1QcjaHWZWbdvJceVz+W32LdoONP2jwAhHN2w57JMB4uiTKN1iFkEXSP1PJgv/vaWDearcJc5VLvFEtDLYklWo6OM9Sa27V3WR4n9uhnzYiheyE81NJeZe4O9JjFnJhQamjBfF/Sil4I3zUMHrvTzRjexZ3JOiGim7qFn1M7kIZNq7O/gkz6K0z/a4k4HF/9CmW6dXieuo0zYfjgYSNySTapAfbAdyoGS47K75XY2PfzzMS2CpdQKk+M3oYJcdFyldKgDXgFUJu7SzUzS/Tb/nrjRA81I8JU+liiNulIWmxAILcd6pL+f405G05rAoa4qdOLvUJwbR8MVlsD6Fu7ef3AZkTos+F6q0kvnk+EzKgpwmKoCaD2S7HKpnxLSxQtOzYMBayuPuLLHK+2cmHTbNlQd4XQJgkfhKXYRjMd0Qkp1oHTPd53qeBzOxQhmm6tjD8rp+A/c7IGcM2/NKZw=="
 
-fp :: String
-fp = "f3:b1:76:72:02:0e:4e:8a:d2:55:16:03:4f:ac:c1:2d"
+md5fp, sha256 :: String
+md5fp = "f3:b1:76:72:02:0e:4e:8a:d2:55:16:03:4f:ac:c1:2d"
+sha256 = "DpMMXGF56PZIU3dJalq/vu0g6LNZdOy/fcrsJX57l2I"
